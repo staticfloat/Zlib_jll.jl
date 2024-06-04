745be359f85b1b9967c515f8d2065fcfe1e66811
Elliot Saba <staticfloat@gmail.com>
2024-06-04 23:29:52 +0000

    Zlib v1.0.0

--- src/Zlib_jll.jl
+++ src/Zlib_jll.jl
@@ -1,9 +1,5 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule Zlib_jll
-using Base
-using Base: UUID
-import JLLWrappers
+module Zlib_jll
+using LazyJLLWrappers
+@generate_jll_from_toml()
+end # module Zlib_jll
 
-JLLWrappers.@generate_main_file_header("Zlib")
-JLLWrappers.@generate_main_file("Zlib", UUID("83775a58-1f1d-513f-b197-d71354ab007a"))
-end  # module Zlib_jll
